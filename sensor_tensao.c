be61eae992b8fed7676d4c4501731c3a92e88e5e
Thiago-mabs <105647802+Thiago-mabs@users.noreply.github.com>
2022-05-21 20:02:53 -0300

    Update sensor_tensao.c

--- sensor_tensao.c
+++ sensor_tensao.c
@@ -1,16 +1,17 @@
 const int SensorTensaoPin=A1; //Declarando a porta que o sensor está localizado
-float TensaoEntrada;
 float TensaoSaida;
 float ValorTensao;
+int R1 = 30000;
+int R2 = 7500;
 void setup(){
     Serial.begin(9600);
 }
 void loop(){
     ValorTensao = analogRead(SensorTensaoPin); //Leitura analógica do valor de entrada do sensor
-    TensaoSaida = (ValorTensao * 5)/1024;     // Conversão para o valor de sáida (variando de 0-5v)
-    TensaoEntrada = TensaoSaida * 5;         // Convesão para o valor de entrada (valor real)
+    TensaoSaida = (ValorTensao * 5)/1024; // Conversão para o valor de sáida (variando de 0-5v)
+    TensaoSaida = TensaoSaida * (R2/(R1+R2));
     Serial.print("Tensão: ");
-    Serial.print(TensaoSaida);
+    Serial.print(TensaoSaida, 2);
     Serial.print("V"); 
     delay(1000);
 }
